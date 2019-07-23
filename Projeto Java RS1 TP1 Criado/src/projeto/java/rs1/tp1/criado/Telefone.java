78104fa8018122da8523bd7b167b9d78b6e337fe
ArthurAzeredo <arthursantosazeredo1@gmail.com>
2019-07-23 16:46:43 -0300

    Projeto Java RS1 TP1 Criado. Aluno Criado. Telefone Criado e Adicionado ao Aluno Disciplina criada e adicionada ao Aluno. Campos com encapsulamento

---
+++ Projeto Java RS1 TP1 Criado/src/projeto/java/rs1/tp1/criado/Telefone.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto.java.rs1.tp1.criado;
+
+/**
+ *
+ * @author aluno
+ */
+public class Telefone {
+    String DDD;
+    String número;
+    String operadora;
+    
+}
